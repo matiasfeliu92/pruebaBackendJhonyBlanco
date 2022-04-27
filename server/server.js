afe6aa400a208df30e0031b97c7dca8220289767
matiasfeliu92 <matumazparrote@gmail.com>
2022-04-27 13:35:23 -0300

    configuracion de la base de datos con las variables de entorno

--- server/server.js
+++ server/server.js
@@ -1,2 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const server = express()
+
+server.set('port', process.env.PORT || 4000)
+
+server.use(morgan('dev'))
+
+server.use(express.urlencoded({extended: false}))
+server.use(express.json())
+
+server.listen(server.get('port'), () => {
+    console.log("http://localhost:"+server.get('port'))
+})
